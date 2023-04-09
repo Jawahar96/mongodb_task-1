be05b569a495d524f17cdbec83c80fa42d62446e
Jawahar96 <jawaharsabesan@gmail.com>
2023-04-09 10:01:33 +0530

    first commit

---
+++ Createdb/db.js
@@ -0,0 +1,71 @@
+[
+    {
+        "id": "1",
+        "product_name": "Intelligent Fresh Chips",
+        "product_price": 655.00,
+        "product_material": "Concrete",
+        "product_color": "mint green"
+    },
+    {
+        "id": "2",
+        "product_name": "Practical Fresh Sausages",
+        "product_price": 911.0,
+        "product_material": "Cotton",
+        "product_color": "indigo"
+    },
+    {
+        "id": "3",
+        "product_name": "Refined Steel Car",
+        "product_price": 690.00,
+        "product_material": "Rubber",
+        "product_color": "gold"
+    },
+    {
+        "id": "4",
+        "product_name": "Gorgeous Plastic Pants",
+        "product_price": 492.00,
+        "product_material": "Soft",
+        "product_color": "plum"
+    },
+    {
+        "id": "5",
+        "product_name": "Sleek Cotton Chair",
+        "product_price": 33.00,
+        "product_material": "Fresh",
+        "product_color": "black"
+    },
+    {
+        "id": "6",
+        "product_name": "Awesome Wooden Towels",
+        "product_price": 474.00,
+        "product_material": "Plastic",
+        "product_color": "orange"
+    },
+    {
+        "id": "7",
+        "product_name": "Practical Soft Shoes",
+        "product_price": 500.00,
+        "product_material": "Rubber",
+        "product_color": "pink"
+    },
+    {
+        "id": "8",
+        "product_name": "Incredible Steel Hat",
+        "product_price": 78.00,
+        "product_material": "Rubber",
+        "product_color": "violet"
+    },
+    {
+        "id": "9",
+        "product_name": "Awesome Wooden Ball",
+        "product_price": 28.00,
+        "product_material": "Soft",
+        "product_color": "azure"
+    },
+    {
+        "id": "10",
+        "product_name": "Generic Wooden Pizza",
+        "product_price": 84.00,
+        "product_material": "Frozen",
+        "product_color": "indigo"
+    }]
